19ec8941999022b8f7dd494307c57475911f227c
Korgin-Artem <korgin7704@mail.ru>
2023-11-29 20:13:54 +0300

    Убрал глобальные переменные и доработал функцию декодирования

--- DataCompessionHW/DataCompessionHW/Korgin_LZW.cpp
+++ DataCompessionHW/DataCompessionHW/Korgin_LZW.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <unordered_map>
+#include <string>
+
 #include "KORGIN_LZW.h"
-std::unordered_map<std::string, int> encoding_map;
-std::unordered_map<int, std::string> decoding_map;
 
 std::string KORGIN_LZW(const std::string& input_data) {
+    std::unordered_map<std::string, int> encoding_map;
     std::string encoded_str;
 
     for (int i = 0; i <= 255; ++i) {
         std::string char_to_str = std::string(1, char(i));
         encoding_map[char_to_str] = i;
-        decoding_map[i] = char_to_str;
     }
 
     std::string current_sequence;
     for (char input_char : input_data) {
-        std::string next_char = std::string(1, input_char);
-        std::string concatenated = current_sequence + next_char;
+        std::string concatenated = current_sequence + input_char;
         if (encoding_map.find(concatenated) != encoding_map.end()) {
             current_sequence = concatenated;
         } else {
             encoded_str += std::to_string(encoding_map[current_sequence]) + " ";
             encoding_map[concatenated] = static_cast<int>(encoding_map.size());
-            decoding_map[static_cast<int>(decoding_map.size())] = concatenated;
-            current_sequence = next_char;
+            current_sequence = input_char;
         }
     }
 
@@ -35,6 +33,7 @@ std::string KORGIN_LZW(const std::string& input_data) {
 }
 
 std::string KORGIN_LZW_Decode(const std::string& encoded_str) {
+    std::unordered_map<int, std::string> decoding_map;
     std::string decoded_str;
 
     for (int i = 0; i <= 255; ++i) {
@@ -42,29 +41,40 @@ std::string KORGIN_LZW_Decode(const std::string& encoded_str) {
         decoding_map[i] = char_to_str;
     }
 
-    std::string output;
-    int code;
-    std::string current_sequence;
+    int prev_code = -1;
+    std::string entry;
+    std::string code;
 
     for (char c : encoded_str) {
         if (c != ' ') {
-            output += c;
+            code += c;
         } else {
-            code = std::stoi(output);
-            if (decoding_map.find(code) == decoding_map.end()) {
-                current_sequence = decoding_map[std::stoi(output.substr(0, output.size() - 1))];
-                current_sequence += current_sequence[0];
+            int current_code = std::stoi(code);
+            if (decoding_map.find(current_code) != decoding_map.end()) {
+                entry = decoding_map[current_code];
             } else {
-                current_sequence = decoding_map[code];
+                entry = decoding_map[prev_code] + decoding_map[prev_code][0];
             }
-            decoded_str += current_sequence;
+            decoded_str += entry;
 
-            if (decoding_map.size() < 4096) {
-                decoding_map[static_cast<int>(decoding_map.size())] = decoding_map[std::stoi(output.substr(0, output.size() - 1))] + current_sequence[0];
+            if (prev_code != -1) {
+                decoding_map[decoding_map.size()] = decoding_map[prev_code] + entry[0];
             }
 
-            output = "";
+            prev_code = current_code;
+            code = "";
         }
+    }
+
+    if (!code.empty()) {
+        int current_code = std::stoi(code);
+        std::string entry;
+        if (decoding_map.find(current_code) != decoding_map.end()) {
+            entry = decoding_map[current_code];
+        } else {
+            entry = decoding_map[prev_code] + decoding_map[prev_code][0];
+        }
+        decoded_str += entry;
     }
 
     return decoded_str;
